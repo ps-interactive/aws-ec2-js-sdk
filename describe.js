c6efccb67bed2fd3d86055545edb6b62ec0aa18a
Tom Bell <tjbell@gmail.com>
2020-05-14 16:46:32 -0600

    fix: remove params

--- describe.js
+++ describe.js
@@ -4,8 +4,8 @@ AWS.config.region = 'us-west-2';
 AWS.config.apiVersions = { ec2: '2016-11-15' };
 
 const ec2 = new AWS.EC2();
-const params = { DryRun: false };
-ec2.describeInstances(params, (err, data) => {
+
+ec2.describeInstances((err, data) => {
   if (err) { console.log('Error', err.stack); }
   else { console.log('Success', JSON.stringify(data)); }
 });
