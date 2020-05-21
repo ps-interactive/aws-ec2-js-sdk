043faf9532b39f4228f95c154d3bc33fbdacc2a6
Tom Bell <tjbell@gmail.com>
2020-05-21 16:01:37 -0600

    fix: change name

--- ec2.js
+++ ec2.js
@@ -1,7 +1,5 @@
 const command = process.argv[2].toUpperCase()
-const process = (err, data) => {
+const process_data = (err, data) => {
   if (err) { console.log(`Error: ${err.message}`); }
   else if (data) { console.log(`Success: ${JSON.stringify(data)}`); }
 }
-
-
