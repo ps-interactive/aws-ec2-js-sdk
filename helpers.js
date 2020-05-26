a81f92f8626aa4beda5b810ee95a55f446f0609d
Tom Bell <tjbell@gmail.com>
2020-05-26 17:01:21 -0600

    feat: async keyExists

--- helpers.js
+++ helpers.js
@@ -15,3 +15,37 @@ const message = (err, data) => {
 };
 
 module.exports = { command, message }
+
+
+// const aws = require('aws-sdk');
+// const command = require('minimist')(process.argv.slice(2))._[0]
+
+// aws.config = {
+//   region: 'us-west-2',
+//   apiVersions: {
+//     ec2: '2016-11-15'
+//   }
+// };
+// const ec2 = new aws.EC2();
+
+
+// function getAwsSecret() {
+//   return ec2.describeKeyPairs().promise();
+// }
+
+// async function getAwsSecretAsync () {
+//   try {
+//     const response = await getAwsSecret();
+//     return response;
+//   } catch (error) {
+//     console.error('Error occurred while retrieving AWS secret');
+//     console.error(error);
+//   }
+// }
+
+// async function asyncExample () {
+//   const secret = await getAwsSecretAsync();
+//   console.debug(secret);
+// }
+
+// asyncExample();
