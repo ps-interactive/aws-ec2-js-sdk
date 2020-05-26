a74f1b962124ff0c4bafacaabfe699e851eda3e5
Tom Bell <tjbell@gmail.com>
2020-05-26 16:04:31 -0600

    fix: use commas

--- ec2.js
+++ ec2.js
@@ -3,9 +3,9 @@ const { command, message, keyExists } = require('./helpers')
 
 const aws = require('aws-sdk');
 aws.config = {
-  region: 'us-west-2';
-  apiVersions: { 
-    ec2: '2016-11-15' 
+  region: 'us-west-2',
+  apiVersions: {
+    ec2: '2016-11-15'
   }
 };
 const ec2 = new aws.EC2();
