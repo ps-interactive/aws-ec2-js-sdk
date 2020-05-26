a54e6bec2c1adecc4961f0101eeb7db7c799bc4e
Tom Bell <tjbell@gmail.com>
2020-05-26 16:42:46 -0600

    fix: remove keyExists

--- ec2.js
+++ ec2.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const { command, message, keyExists } = require('./helpers')
+const { command, message } = require('./helpers')
 
 const aws = require('aws-sdk');
 aws.config = {
@@ -12,17 +12,7 @@ const ec2 = new aws.EC2();
 
 switch (command) {
   case 'key':
-    const keyParams = { KeyName: 'ec2-js-sdk-key-pair' };
-    if(keyExists(keyParams.KeyName)){
-      console.log('A Key Pair already exists.');
-    } else {
-      ec2.createKeyPair(keyParams, (err, data) => {
-        if (err) { console.log(`Error: ${err.message}`); }
-        else if (data) {
-          fs.writeFileSync('private.pem', data.KeyMaterial, 'utf8');
-        }
-      });
-    }
+    console.log('key');
     break;
   default:
     console.error('Not a valid command!');
--- helpers.js
+++ helpers.js
@@ -1,10 +1,17 @@
+const aws = require('aws-sdk');
 const command = require('minimist')(process.argv.slice(2))._[0]
+
+aws.config = {
+  region: 'us-west-2',
+  apiVersions: {
+    ec2: '2016-11-15'
+  }
+};
+const ec2 = new aws.EC2();
 
 const message = (err, data) => {
   if (err) { console.log(`Error: ${err.message}`); }
   else if (data) { console.log(`Success: ${JSON.stringify(data)}`); }
 };
 
-const keyExists = ec2.describeKeyPairs().promise().then((data) => data).catch((err) => err);
-
-module.exports = { command, message, keyExists }
+module.exports = { command, message }
