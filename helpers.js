4d4267f51a09a564b52c168dd2fcdb00365b8d80
Tom Bell <tjbell@gmail.com>
2020-06-03 10:22:52 -0600

    solution: all challenges

--- helpers.js
+++ helpers.js
@@ -1,14 +1,16 @@
-const aws = require('aws-sdk');
-const command = require('minimist')(process.argv.slice(2))._[0]
 const _ = require("lodash");
+const AWS = require('aws-sdk');
+const parse = require('minimist')(process.argv.slice(2));
 
-aws.config = {
+const command = parse._[0];
+const instance = parse._[1];
+const tag = parse._[2];
+
+AWS.config = {
   region: 'us-west-2',
-  apiVersions: {
-    ec2: '2016-11-15'
-  }
+  apiVersions: { ec2: '2016-11-15' }
 };
-const ec2 = new aws.EC2();
+const ec2 = new AWS.EC2();
 
 const message = (err, data) => {
   if (err) { console.log(`Error: ${err.message}`); }
@@ -19,10 +21,10 @@ const getKey = async name => {
   let data = await ec2.describeKeyPairs().promise();
   let result = _.filter(data.KeyPairs, { KeyName: name })
   return result.length == 1;
-}
+};
 
 const keyExists = async (name, func) => !await getKey(name) ? func() : console.log("A key exists with that name.");
 const ensureKey = async (name, func) => await getKey(name) ? func() : console.log("A key doesn't exists with that name.");
 
 
-module.exports = { command, message, keyExists, ensureKey }
+module.exports = { command, instance, tag, message, keyExists, ensureKey };
